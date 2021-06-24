b878eec1ed8ba4a3a06a120f4abee3f00726227e
changlin93 <767425412@qq.com>
2021-06-24 18:40:05 +0800

    feat: 配置项目规范

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,20 @@
 <template>
-  <div>
-    <n-button>按钮</n-button>
-  </div>
+    <div>
+        <n-button>按钮</n-button>
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { NButton } from 'naive-ui'
 export default defineComponent({
-  components:{
-    NButton,
-  },
-  setup () {
-    
-
-    return {}
-  }
+    components: {
+        NButton
+    },
+    setup() {
+        return {}
+    }
 })
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
