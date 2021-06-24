97ff9bcef5ec9b255f137e9ba3c64a14a3554739
changlin93 <767425412@qq.com>
2021-06-24 18:31:51 +0800

    feat: 集成naive-ui组件库

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div>
+    <n-button>按钮</n-button>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
+import { NButton } from 'naive-ui'
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    NButton,
+  },
+  setup () {
+    
+
+    return {}
   }
 })
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
 </style>
